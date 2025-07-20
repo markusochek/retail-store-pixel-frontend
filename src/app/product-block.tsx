a2123010a76ac720b356bd78cb2390c3afb2df4e
sagalaev mark <sagalaev.m@gmail.com>
2025-07-20 19:28:40 +0400

    add read products txt and display on product-container.tsx

--- src/app/product-block.tsx
+++ src/app/product-block.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 const ProductBlock = ({ id, name, salePrice }: {id: number, name: string, salePrice: number}) => {
     return (
         <>
-            <div style={{display: "flex", flexDirection: "column", whiteSpace: "pre-line"}}>
-                <img height={"100px"} width={"100px"} src="37522b69_4c36_11f0_84a5_0cc47adeeeb3_2465ba59_4cce_11f0_84a5_0cc47adeeeb3.webp" alt="error loaded"/>
+            <div style={{display: "flex", flexDirection: "column", whiteSpace: "pre-line", width: "18%"}}>
+                <img src="37522b69_4c36_11f0_84a5_0cc47adeeeb3_2465ba59_4cce_11f0_84a5_0cc47adeeeb3.webp" alt="error loaded"/>
                 <span>Код: {id}</span>
-                <span>{name}</span>
-                <span>Розничная цена {salePrice}₽</span>
+                <span style={{display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical',overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '200px',}}>{name}</span>
+                <span>Розничная цена</span>
+                <span>{salePrice}₽</span>
             </div>
         </>
     );

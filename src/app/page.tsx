7ab27cbc4ad1812fb4ede0e2e8df7e13a5ca434a
sagalaev mark <sagalaev.m@gmail.com>
2025-07-27 21:49:58 +0400

    add loop requestProductsFile and create productFromFile once per second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,74 @@
 import Filter from "@/app/filter";
 import ProductContainer from "@/app/product-container";
+import {diContainer} from "@/app/lib/di/di-container";
+import fs from "node:fs";
 
-export default function Home() {
-  return (
-    <>
-      <div style={{ width: "100%",height: "100%", display: "flex", flexDirection: "row", alignItems: "center" }}>
-          <Filter></Filter>
-          <ProductContainer></ProductContainer>
-      </div>
-    </>
-  );
+function parseProductsFile(filename: fs.PathOrFileDescriptor) {
+    try {
+        const content = fs.readFileSync(filename, 'utf8');
+
+        const lines = content.split('\n')
+            .map(line => line.trim())
+            .filter(line => line !== '');
+
+        if (lines.length < 2) {
+            throw new Error('Файл не содержит данных или заголовков');
+        }
+
+        const headers = lines[0].split(/\t|\s{2,}/).filter((part: string) => part !== '');
+        const products = [];
+
+        for (let i = 1; i < lines.length; i++) {
+            const parts = lines[i].split(/\t|\s{2,}/).filter((part: string) => part !== '');
+
+            if (parts.length !== headers.length) {
+                console.warn(`Строка ${i} имеет неверное количество колонок:`, parts);
+                continue;
+            }
+
+            try {
+                products.push({
+                    id_from_another_db: +parts[0],
+                    name: parts[1],
+                    unitOfMeasurement: parts[2],
+                    salePrice: parseFloat(parts[3].replace(/\s/g, '').replace(',', '.')),
+                    quantity: parseFloat(parts[4].replace(',', '.'))
+                });
+            } catch (e: any) {
+                console.warn(`Ошибка обработки строки ${i}:`, e.message);
+            }
+        }
+
+        return products;
+    } catch (e: any) {
+        console.error('Ошибка парсинга файла:', e.message);
+        return [];
+    }
+}
+
+export default async function Home() {
+    const productService = await diContainer.getProductService();
+
+    const productsFromFile = parseProductsFile('products');
+    for (let productFromFile of productsFromFile) {
+        await productService.create(productFromFile);
+    }
+
+    const intervalRequestProductsFile = setInterval(async () => {
+        const productService = await diContainer.getProductService();
+
+        const productsFromFile = parseProductsFile('products');
+        for (let productFromFile of productsFromFile) {
+            await productService.create(productFromFile);
+        }
+    }, 1000);
+
+    return (
+        <>
+            <div style={{width: "100%", height: "100%", display: "flex", flexDirection: "row", alignItems: "center"}}>
+                <Filter></Filter>
+                <ProductContainer></ProductContainer>
+            </div>
+        </>
+    );
 }
--- src/app/product-container.tsx
+++ src/app/product-container.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import ProductBlock from "@/app/product-block";
-import { ProductService } from './services/product.service';
 
-import * as fs from "node:fs";
 import {getImagePath} from "@/app/lib/helpers/images";
 import {diContainer} from "@/app/lib/di/di-container";
 
 const ProductContainer = async () => {
     const productService = await diContainer.getProductService();
-
-    const productsFromFile = parseProductsFile('products');
-    for (let productFromFile of productsFromFile) {
-        await productService.create(productFromFile);
-    }
-
     const products:any[] = await productService.findAll();
 
     return (
@@ -40,48 +32,5 @@ const ProductContainer = async () => {
         </>
     );
 };
-
-function parseProductsFile(filename: fs.PathOrFileDescriptor) {
-    try {
-        const content = fs.readFileSync(filename, 'utf8');
-
-        const lines = content.split('\n')
-            .map(line => line.trim())
-            .filter(line => line !== '');
-
-        if (lines.length < 2) {
-            throw new Error('Файл не содержит данных или заголовков');
-        }
-
-        const headers = lines[0].split(/\t|\s{2,}/).filter((part: string) => part !== '');
-        const products = [];
-
-        for (let i = 1; i < lines.length; i++) {
-            const parts = lines[i].split(/\t|\s{2,}/).filter((part: string) => part !== '');
-
-            if (parts.length !== headers.length) {
-                console.warn(`Строка ${i} имеет неверное количество колонок:`, parts);
-                continue;
-            }
-
-            try {
-                products.push({
-                    id_from_another_db: +parts[0],
-                    name: parts[1],
-                    unitOfMeasurement: parts[2],
-                    salePrice: parseFloat(parts[3].replace(/\s/g, '').replace(',', '.')),
-                    quantity: parseFloat(parts[4].replace(',', '.'))
-                });
-            } catch (e: any) {
-                console.warn(`Ошибка обработки строки ${i}:`, e.message);
-            }
-        }
-
-        return products;
-    } catch (e: any) {
-        console.error('Ошибка парсинга файла:', e.message);
-        return [];
-    }
-}
 
 export default ProductContainer;

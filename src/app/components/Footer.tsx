6e52544a0bc64974568d1032539b0dae5f69aa7a
sagalaev mark <sagalaev.m@gmail.com>
2025-09-28 19:36:19 +0400

    upd logo footer in logo-white.svg

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import logo from '../../../public/icons/logo.svg';
+import logoWhite from '../../../public/icons/logo-white.svg';
 
 const Footer = () => {
   return (
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Лого и описание */}
           <div className="flex flex-col items-center md:items-start">
-            <Image src={logo} alt="Pixel" className={'w-60 h-16'} />
+            <Image src={logoWhite} alt="Pixel" className={'w-60 h-16'} />
             <p className="mt-4 text-gray-400 text-sm text-center md:text-left">
               Интернет-магазин игрушек и канцтоваров
             </p>
